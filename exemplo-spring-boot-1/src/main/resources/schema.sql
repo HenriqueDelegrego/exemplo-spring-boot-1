07dba27c1117951ce6008411fa7e07a99d635a08
Henrique <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-06-15 19:55:22 -0300

    fix: move schema.sql para resources

---
+++ exemplo-spring-boot-1/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE produto
+(
+id INTEGER,
+nm_produto VARCHAR(50),
+preco DECIMAL(10,2),
+PRIMARY KEY(id)
+);
--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE produto
-(
-id INTEGER,
-nm_produto VARCHAR(50),
-preco DECIMAL(10,2),
-PRIMARY KEY(id)
-);
